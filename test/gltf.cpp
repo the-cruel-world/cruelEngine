aacc8b67d437fd3dc960dafd05c1c7eb680d8c65
yiwenxue <15225434259xue@gmail.com>
2021-04-11 13:08:47 +0300

    Add test codes for gltf.

--- test/gltf.cpp
+++ test/gltf.cpp
@@ -1,10 +1,12 @@
-#define TINYGLTF_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-
+#include "scene/scene_header.h"
 #include "tiny_gltf.h"
 
+#include <chrono>
+#include <iostream>
+
 using namespace tinygltf;
+using namespace std;
+using namespace cruelEngine;
 
 int main(int argc, char const *argv[])
 {
@@ -21,8 +23,9 @@ int main(int argc, char const *argv[])
     std::string err;
     std::string warn;
 
+    auto start_marker = std::chrono::high_resolution_clock::now();
+
     bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, argv[1]);
-    // bool ret = loader.LoadBinaryFromFile(&model, &err, &warn, argv[1]); // for binary glTF(.glb)
 
     if (!warn.empty())
     {
@@ -39,6 +42,33 @@ int main(int argc, char const *argv[])
         printf("Failed to parse glTF\n");
         return -1;
     }
+    cout << "model loaded" << endl;
 
+    float duration = std::chrono::duration<float, std::chrono::milliseconds::period>(
+                         std::chrono::high_resolution_clock::now() - start_marker)
+                         .count();
+
+    cout << "Time usage: " << duration << " ms" << endl;
+
+    cruelScene::Scene scene("Myscene");
+
+    std::vector<std::unique_ptr<cruelScene::Node>> nodes{};
+
+    cout << "Nodes: " << model.nodes.size() << endl;
+
+    for (size_t node_index = 0; node_index < model.nodes.size(); ++node_index)
+    {
+        auto &gltf_node  = model.nodes[node_index];
+        auto  scene_node = make_unique<cruelScene::Node>(node_index, gltf_node.name);
+
+        cout << "Node " << node_index << ": " << gltf_node.name << endl;
+        nodes.push_back(std::move(scene_node));
+    }
+
+    scene.set_root_node(*nodes.at(0));
+
+    scene.add_nodes(std::move(nodes));
+
+    cout << "scene created" << endl;
     return 0;
 }
